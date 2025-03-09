7f79966cd35051366f1d5e1fe75f52908c8a0ee4
Tamal Sarkar <77930336+KARDT89@users.noreply.github.com>
2025-03-09 21:17:56 +0530

    Time: 8 ms (71.78%), Space: 58.8 MB (19.95%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -4,14 +4,13 @@
  */
 var lengthOfLongestSubstring = function (s) {
     let charSet = new Set()
-    if(s.length === 0) return 0
-
     let left = 0
     let result = 0
-    for (let right = 0; right < s.length-1; right++) {
+    if (s.length === 0) return 0;
+    for (let right = 0; right < s.length; right++) {
         while (charSet.has(s[right])) {
             charSet.delete(s[left])
-            left += 1
+            left++
         }
         charSet.add(s[right])
         result = Math.max(result, right - left + 1)
