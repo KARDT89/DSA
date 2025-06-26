519ea4921e08159258a78bc414ed90532dd77107
Tamal Sarkar <77930336+KARDT89@users.noreply.github.com>
2025-06-26 20:18:22 +0530

    Time: 2 ms (99.25%), Space: 55.4 MB (94.33%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,6 +4,31 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-    return s.split("").sort().join("") === t.split("").sort().join("");
-    
+    // edge case
+    if (s.length !== t.length) return false
+
+    // the container
+    let hashMap = {}
+
+    // loop through s and store it in container
+    for (let i = 0; i < s.length; i++) {
+        let letter = s[i]
+        if (!hashMap[letter]) {
+            hashMap[letter] = 1
+        } else {
+            hashMap[letter]++
+        }
+    }
+
+    // loop through t and check in container if the current letter is present.
+    // If present, decrease the count. U get the point. EZ
+    for (let j = 0; j < t.length; j++) {
+        let letter = t[j]
+        if (hashMap[letter] === undefined) return false
+        if (hashMap[letter] < 1) return false
+        hashMap[letter]--
+    }
+
+    return true
+
 };
