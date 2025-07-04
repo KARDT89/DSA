406ed0c7047ba80a4e215f79f18d580d18a09120
Tamal Sarkar <77930336+KARDT89@users.noreply.github.com>
2025-07-04 10:04:16 +0530

    Time: 27 ms (80.93%), Space: 66.1 MB (91.56%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -2,15 +2,15 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function (strs) {
-    let sorted = strs.map((val) => val.split("").sort().join(""));
-    let hashMap = {};
-    for (let i = 0; i < sorted.length; i++) {
-        if (sorted[i] in hashMap) {
-            hashMap[sorted[i]].push(strs[i])
-        } else {
-            hashMap[sorted[i]] = [strs[i]]
-        }
-    }
-    return Object.values(hashMap)
+var groupAnagrams = function(strs) {
+    let prevMap = {}
+   
+    for(let i=0; i<strs.length; i++){
+        let sorted = strs[i].split("").sort().join()
+        if(sorted in prevMap){
+            prevMap[sorted].push(strs[i])
+        }else{
+            prevMap[sorted] = [strs[i]]
+        } 
+    } return Object.values(prevMap)
 };
