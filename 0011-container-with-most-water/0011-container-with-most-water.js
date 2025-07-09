79802fa8d7d7abcb35a02af2c17d25a8cc8feddc
Tamal Sarkar <77930336+KARDT89@users.noreply.github.com>
2025-07-09 11:26:31 +0530

    Time: 2 ms (81.3%), Space: 62.5 MB (62.65%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,17 +3,14 @@
  * @return {number}
  */
 var maxArea = function (height) {
+    let left = 0
+    let right = height.length - 1
     let maxArea = 0
-    let left = 0, right = height.length - 1
+
     while (left < right) {
         let area = Math.min(height[left], height[right]) * (right - left)
         maxArea = Math.max(area, maxArea)
-
-        if (height[left] < height[right]) {
-            left++
-        } else {
-            right--
-        }
+        height[left] < height[right] ? left++ : right--
     }
     return maxArea
 };
