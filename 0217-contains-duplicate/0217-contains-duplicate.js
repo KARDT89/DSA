31933cd072fd82bd6e7b7af6103e3d21586fb8e5
Tamal Sarkar <77930336+KARDT89@users.noreply.github.com>
2025-11-05 03:06:45 +0530

    Time: 19 ms (60.08%), Space: 72.4 MB (67.67%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,12 +3,5 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const prevSet = new Set()
-    for(let i=0;i<nums.length;i++){
-        if(prevSet.has(nums[i])){
-            return true
-        }else{
-            prevSet.add(nums[i])
-        }
-    } return false
+    return new Set(nums).size !== nums.length
 };
