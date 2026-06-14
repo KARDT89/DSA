2b484a70a0fa179cfad560a4f20176587bd07914
Tamal Sarkar <77930336+KARDT89@users.noreply.github.com>
2026-06-14 13:10:09 +0530

    Time: 4 ms (50.85%), Space: 56.3 MB (16.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,16 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    let prevMap = {} // A hashmap to store numbers and their indices
-    for (let i = 0; i < nums.length; i++) {
-        let needed = target - nums[i] // Calculate the required number to form the sum
-
-        // Check if the needed number exists in the hashmap
-        if (needed in prevMap) {
-            return [prevMap[needed], i]; // If found, return the indices
-        } else {
-            // Otherwise, store the current number and its index in the hashmap
-            prevMap[nums[i]] = i;
+    let hash = {}
+    for (let [index, val] of nums.entries()) {
+        let result = target - val
+        if (result in hash) {
+            return [index, hash[result]]
         }
-    } return
+        hash[val] = index
+    }
+    return false
 };
