ef6aed3d28fb48799b4a13dd70928c0e7b748205
Tamal Sarkar <77930336+KARDT89@users.noreply.github.com>
2026-07-16 16:54:48 +0530

    Time: 0 ms (100%), Space: 53.7 MB (60.21%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,23 +3,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-    //Brute force
-    let count = new Array(3).fill(0)
-
-    // Count occurrences
-    for (const v of nums) {
-        count[v]++
-    }
-    // Overwrite the array
-    let i = 0;
-
-    for (let value = 0; value <= 2; value++) {
-        while (count[value] > 0) {
-            nums[i] = value;
-            i++;
-            count[value]--;
+    let low = 0, mid = 0, high = nums.length - 1
+    while (mid <= high) {
+        if (nums[mid] === 0) {
+            [nums[mid], nums[low]] = [nums[low], nums[mid]]
+            mid++
+            low++
+        } else if (nums[mid] === 1) {
+            mid++
+        } else {
+            [nums[high], nums[mid]] = [nums[mid], nums[high]]
+            high--
         }
     }
-
-    return nums;
 };
